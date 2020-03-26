c4b4788d685e79ef0169681ff4f59f8e464d64ed
Matisse <matisse>
2020-03-26 15:32:52 +0100

    made clear difference is in %

--- panda.py
+++ panda.py
@@ -4,6 +4,6 @@ df = pd.read_csv("data.csv")
 
 
 table = pd.crosstab(index=df['ev'], columns=df['year'])
-table['difference'] = ((table[2019] - table[2018]) / table[2018] * 100)
+table['difference in %'] = ((table[2019] - table[2018]) / table[2018] * 100)
 
 print(table)
