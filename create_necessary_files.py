28a354b5102ec9f53a68a04f8d2c9a4c226aa1d3
Matisse <matisse>
2020-03-30 18:08:12 +0200

    Added comments

--- create_necessary_files.py
+++ create_necessary_files.py
@@ -1,4 +1,4 @@
-# Create the necesairy csv files
+# Create the necessary csv files
 
 import csv
 
@@ -8,11 +8,19 @@ def export_csv():
         csv_reader = csv.reader(csv_file, delimiter=',')
         with open('data.csv', 'w', newline='') as file:
             writer = csv.writer(file)
+            # create column names for dataset
             writer.writerow(["kenteken", "ev", "year"])
+            kentekens = []
             for row in csv_reader:
+                # row 11 and 12 empty to check if is ev and row 20 = 21 for only non imported cars
                 if row[11] == '' and row[12] == '' and row[20] == row[21]:
+                    # write to our dataset the license row0 True (is an ev) and the year
                     writer.writerow([row[0], True, str(row[21])[:4]])
-                elif row[0] != "Kenteken" and row[20] == row[21]:
+                    kentekens.append(row[0])
+                # row0 not equal to kenteken because that is column name and same as above for 20 and 21s
+                # row0 not in kentekens to skip the evs
+                elif row[0] != "Kenteken" and row[20] == row[21] and row[0] not in kentekens:
+                    # write to dataset row0 --> license false for no ev and the year
                     writer.writerow([row[0], False, str(row[21])[:4]])
 
 
--- panda.py
+++ panda.py
@@ -1,9 +1,14 @@
 import pandas as pd
 
+#read the dataset
 df = pd.read_csv("data.csv")
 
-
+# Create crosstab
 table = pd.crosstab(index=df['ev'], columns=df['year'])
+# add difference in % to crosstab
 table['difference in %'] = ((table[2019] - table[2018]) / table[2018] * 100)
+# add absolute difference to crosstab
+table['absolute difference'] = (table[2019] - table[2018])
 
+# print crosstab
 print(table)
